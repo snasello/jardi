b582e6138ef6ab327a5eb30b3b003936196cf0db
Samuel Nasello <samuel_nasello@yahoo.Fr>
2013-10-02 22:00:28 +0200

    init

---
+++ jardi-parent/jardi-datas/jardi-datas-common/src/main/java/fr/snasello/jardi/datas/model/Graine.java
@@ -0,0 +1,65 @@
+package fr.snasello.jardi.datas.model;
+
+public class Graine {
+	
+	private String familleBotanique;
+	
+	private String familleLegumes;
+	
+	private String familleGourmande;
+	
+	private String type;
+	
+	private String nom;
+	
+	private String variete;
+
+	public String getFamilleBotanique() {
+		return familleBotanique;
+	}
+
+	public void setFamilleBotanique(String familleBotanique) {
+		this.familleBotanique = familleBotanique;
+	}
+
+	public String getFamilleLegumes() {
+		return familleLegumes;
+	}
+
+	public void setFamilleLegumes(String familleLegumes) {
+		this.familleLegumes = familleLegumes;
+	}
+
+	public String getFamilleGourmande() {
+		return familleGourmande;
+	}
+
+	public void setFamilleGourmande(String familleGourmande) {
+		this.familleGourmande = familleGourmande;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getVariete() {
+		return variete;
+	}
+
+	public void setVariete(String variete) {
+		this.variete = variete;
+	}
+	
+}
---
+++ jardi-parent/jardi-datas/jardi-datas-common/src/main/java/fr/snasello/jardi/datas/model/Jardinier.java
@@ -0,0 +1,55 @@
+package fr.snasello.jardi.datas.model;
+
+public class Jardinier {
+
+	private String id;
+	
+	private String login;
+	
+	private String email;
+	
+	private String prenom;
+	
+	private String nom;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+}
